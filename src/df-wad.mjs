2019f58adc3cc1b368eb405870b11563aaba7aa5
Max Brylov <max.brylov@protonmail.com>
2022-05-26 11:56:02 -0700

    promisify dfwad creation and add zip support

--- src/df-wad.mjs
+++ src/df-wad.mjs
@@ -1,5 +1,7 @@
 import { getExtensionFromBuffer, wadToJSON } from './utility.mjs'
 import { inflate } from './pako.esm.mjs'
+import './jszip.js'
+const Jszip = new JSZip()
 class WadStruct {
   constructor (/** @type {any} */ structObject) {
     if (structObject !== undefined) {
@@ -21,37 +23,74 @@ class Resource {
 }
 
 class DFWad {
-  constructor (/** @type {Uint8Array} */ buffer, /** @type {string} */ name) {
+  constructor (/** @type {Uint8Array} */ buffer) {
     this.buffer = buffer
-    const type = getExtensionFromBuffer(buffer)
-    if (type === 'dfwad') {
-      const wadObject = wadToJSON(buffer)
-      /** @type {WadStruct[]} */ this.structs = []
-      for (const i in wadObject) {
-        const obj = wadObject[i]
-        if (obj === undefined) continue
-        const structObject = new WadStruct(obj)
-        this.structs.push(structObject)
-      }
-      /** @type {Resource[]} */ this.files = []
-      for (const i in this.structs) {
-        const struct = this.structs[i]
-        if (struct === undefined) continue
-        if (struct.type === '') continue
-        const structBuffer = buffer.slice(struct.memAddress, struct.memAddress + struct.memLength)
-        const decompressed = inflate(structBuffer)
-        if (decompressed === undefined || typeof decompressed === 'string') continue
-        const parent = struct.parentSection
-        const resname = struct.name
-        const resource = new Resource(decompressed, (parent === '' ? '' : parent + '/') + resname) // if empty, don't add slash
-        this.files.push(resource)
-      }
-    } else if (type === 'dfzip') {
-      // todo
-    } else {
-      // skip
-    }
+    /** @type {Resource[]} */ this.files = []
+    /** @type {WadStruct[]} */ this.structs = []
   }
 }
 
-export { DFWad }
+function DfwadFrom (/** @type {Uint8Array} */ buffer) {
+  const Dfwad = new DFWad(buffer)
+  const type = getExtensionFromBuffer(buffer)
+  if (type === 'dfwad') {
+    const wadObject = wadToJSON(buffer)
+    for (const i in wadObject) {
+      const obj = wadObject[i]
+      if (obj === undefined) continue
+      const structObject = new WadStruct(obj)
+      Dfwad.structs.push(structObject)
+    }
+    for (const i in Dfwad.structs) {
+      const struct = Dfwad.structs[i]
+      if (struct === undefined) continue
+      if (struct.type === '') continue
+      const structBuffer = buffer.slice(struct.memAddress, struct.memAddress + struct.memLength)
+      const decompressed = inflate(structBuffer)
+      if (decompressed === undefined || typeof decompressed === 'string') continue
+      const parent = struct.parentSection
+      const resname = struct.name
+      const resource = new Resource(decompressed, (parent === '' ? '' : parent + '/') + resname) // if empty, don't add slash
+      Dfwad.files.push(resource)
+    }
+    const /** @type {Promise<DFWad>} */ promise = new Promise((resolve) => {
+      resolve(Dfwad)
+    })
+    return promise
+  } else if (type === 'dfzip') {
+    const /** @type {Promise<DFWad>} */ promise = new Promise((resolve, reject) => {
+      const /** @type {Promise<Resource>[]} */ promises = []
+      Jszip.loadAsync(buffer).then((zip) => {
+        for (const i in zip.files) {
+          const file = zip.files[i]
+          if (file === undefined || file.dir === true) continue
+          const loadPromise = new Promise((resolve, reject) => {
+            file.async('ArrayBuffer').then((/** @type {ArrayBuffer} */ content) => {
+              const view = new Uint8Array(content)
+              const resource = new Resource(view, file.name)
+              Dfwad.files.push(resource)
+              resolve(true)
+            }).catch(() => {
+              reject(Error('Failed to load file in ZIP!'))
+            })
+          })
+          promises.push(loadPromise)
+        }
+        Promise.allSettled(promises).then(() => {
+          resolve(Dfwad)
+        })
+      }).catch(() => {
+        reject(Error('Failed to load ZIP!'))
+      })
+    })
+    return promise
+  } else {
+    // skip
+  }
+  const /** @type {Promise<DFWad>} */ promise = new Promise((resolve) => {
+    resolve(Dfwad)
+  })
+  return promise
+}
+
+export { DFWad, DfwadFrom }
