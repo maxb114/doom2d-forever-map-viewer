3abeab7c814f5f9a09d412f4c3f31235181921b6
Max Brylov <max.brylov@protonmail.com>
2022-05-27 14:40:58 +0300

    add cropImage function

--- src/image.mjs
+++ src/image.mjs
@@ -22,4 +22,27 @@ function convertImage (/** @type {Uint8Array} */ buffer, /** @type {string} */ s
   return promise
 }
 
-export { convertImage }
+function cropImage (/** @type {Uint8Array} */ buffer, /** @type {string} */ sourceExtension, width = 0, height = 0, offsetx = 0, offsety = 0) {
+  const /** @type {Promise<ArrayBuffer>} */ promise = new Promise((resolve, reject) => {
+    const source = 'src' + '.' + sourceExtension
+    const target = 'crop' + '.' + sourceExtension
+    const image = { name: source, content: buffer }
+    const convertCommand = ['convert', source, '-crop', width.toString(10) + 'x' + height.toString(10) + '+' +
+    offsetx.toString(10) + '+' + offsety.toString(10), target]
+    Magick.call([image], convertCommand).then((result) => {
+      if (result.outputFiles.length <= 0 || result.outputFiles[0] === undefined) reject(Error('Error converting image!'))
+      const output = result.outputFiles[0]
+      const /** @type {Blob} */ blob = output.blob
+      blob.arrayBuffer().then((arrayBuffer) => {
+        resolve(arrayBuffer)
+      }).catch((error) => {
+        reject(error)
+      })
+    }).catch((error) => {
+      reject(error)
+    })
+  })
+  return promise
+}
+
+export { convertImage, cropImage }
