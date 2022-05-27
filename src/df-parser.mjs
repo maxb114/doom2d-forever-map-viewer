261fe7af2cef476eb04eeb29567e5a7d17c0dde0
Max Brylov <max.brylov@protonmail.com>
2022-05-27 14:05:55 +0300

    introduce anim texture format parser

--- src/df-parser.mjs
+++ src/df-parser.mjs
@@ -492,4 +492,53 @@ class DFParser {
   }
 }
 
-export { DFParser }
+class DFAnimTextureParser {
+  constructor (/** @type {string} */ content) {
+    this.content = content
+    const lexer = new Tokenizr()
+    lexer.rule(/[+-]?[0-9]+/, (/** @type {any} */ ctx, /** @type {any} */ match) => {
+      ctx.accept('number', parseInt(match[0]))
+    })
+    lexer.rule(/[a-zA-Z][a-zA-Z0-9_.]*/, (/** @type {any} */ ctx) => {
+      ctx.accept('id')
+    })
+    lexer.rule(/=/, (/** @type {any} */ ctx) => {
+      ctx.accept('assignment')
+    })
+    lexer.rule(/\/\/[^\r\n]*\r?\n/, (/** @type {any} */ ctx) => { // comments
+      ctx.ignore()
+    })
+    lexer.rule(/[ \t\r\n]+/, (/** @type {any} */ ctx) => { // whitespace
+      ctx.ignore()
+    })
+    lexer.rule(/./, (/** @type {any} */ ctx) => {
+      ctx.accept('char')
+    })
+    content = content.trim()
+    lexer.input(content)
+    const /** @type {any} */ parsedObject = {}
+    const tokens = lexer.tokens()
+    let /** @type {String} */ state = 'search'
+    let activeElement
+    for (const token of tokens) {
+      if (token.type === 'id' || token.type === 'number' || token.type === 'char') {
+        if (state === 'search') {
+          activeElement = token
+          state = 'assign' // look for assignment symbol
+        } else if (state === 'evaluation') {
+          parsedObject[activeElement.value] = token.value
+          state = 'search'
+        }
+      } else if (token.type === 'assignment') {
+        state = 'evaluation'
+      }
+    }
+    this.parsed = {
+      /** @type {string} */ resource: parsedObject.resource ?? '',
+      /** @type {number} */ frameWidth: parsedObject.framewidth ?? 0,
+      /** @type {number} */ frameHeight: parsedObject.frameheight ?? 0
+    }
+  }
+}
+
+export { DFParser, DFAnimTextureParser }
