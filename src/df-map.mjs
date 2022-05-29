6b0a8ec8650f078a26af7a2155ffd552fb3ba177
Max Brylov <max.brylov@protonmail.com>
2022-05-29 16:01:22 +0300

    add df-map convertation to text map

--- src/df-map.mjs
+++ src/df-map.mjs
@@ -105,6 +105,71 @@ class DFMap {
     }
     return null
   }
+
+  asText () {
+    const start = 'map' + ' {'
+    let body = '\n'
+    body = body + ' '.repeat(2) + 'name' + ' ' + "'" + (this.name === '' ? 'Unnamed' : this.name) + "'" + ';' + '\n'
+    if (this.author !== '') body = body + ' '.repeat(2) + 'author' + ' ' + "'" + this.author + "'" + ';' + '\n'
+    if (this.description !== '') body = body + ' '.repeat(2) + 'description' + ' ' + "'" + this.description + "'" + ';' + '\n'
+    if (this.music !== '') body = body + ' '.repeat(2) + 'music' + ' ' + "'" + this.music + "'" + ';' + '\n'
+    if (this.sky !== '') body = body + ' '.repeat(2) + 'sky' + ' ' + "'" + this.sky + "'" + ';' + '\n'
+    body = body + ' '.repeat(2) + 'size' + ' ' + '(' + (this.size.x ?? 0).toString(10) + ' ' + (this.size.y ?? 0).toString(10) + ')' + ';' + '\n'
+    for (const texture of this.textures) {
+      let msg = ''
+      msg = msg + '\n'
+      msg = msg + ' '.repeat(2) + 'texture' + ' ' + texture.id + ' ' + '{' + '\n'
+      msg = msg + ' '.repeat(4) + 'path' + ' ' + "'" + texture.path + "'" + ';' + '\n'
+      msg = msg + ' '.repeat(4) + 'animated' + ' ' + (texture.animated ? 'true' : 'false') + ';' + '\n'
+      msg = msg + ' '.repeat(2) + '}' + '\n'
+      body = body + msg
+    }
+    for (const panel of this.panels) {
+      let msg = ''
+      msg = msg + '\n'
+      msg = msg + ' '.repeat(2) + 'panel' + ' ' + panel.id + ' ' + '{' + '\n'
+      msg = msg + ' '.repeat(4) + 'position' + ' ' + '(' + (panel.pos.x).toString(10) + ' ' + (panel.pos.y).toString(10) + ')' + ';' + '\n'
+      msg = msg + ' '.repeat(4) + 'size' + ' ' + '(' + (panel.size.width).toString(10) + ' ' + (panel.size.height).toString(10) + ')' + ';' + '\n'
+      msg = msg + ' '.repeat(4) + 'texture' + ' ' + panel.texture + ';' + '\n'
+      msg = msg + ' '.repeat(4) + 'type' + ' ' + panel.type[0] + ';' + '\n'
+      msg = msg + ' '.repeat(4) + 'alpha' + ' ' + (panel.alpha === -1 ? 0 : panel.alpha) + ';' + '\n'
+      msg = msg + ' '.repeat(4) + 'flags' + ' ' + panel.flags.join(' | ') + ';' + '\n'
+      msg = msg + ' '.repeat(2) + '}' + '\n'
+      body = body + msg
+    }
+    for (const item of this.items) {
+      let msg = ''
+      msg = msg + '\n'
+      msg = msg + ' '.repeat(2) + 'item' + ' ' + item.id + ' ' + '{' + '\n'
+      msg = msg + ' '.repeat(4) + 'position' + ' ' + '(' + (item.pos.x).toString(10) + ' ' + (item.pos.y).toString(10) + ')' + ';' + '\n'
+      msg = msg + ' '.repeat(4) + 'type' + ' ' + item.type + ';' + '\n'
+      msg = msg + ' '.repeat(4) + 'options' + ' ' + item.options.join(' | ') + ';' + '\n'
+      msg = msg + ' '.repeat(2) + '}' + '\n'
+      body = body + msg
+    }
+    for (const monster of this.monsters) {
+      let msg = ''
+      msg = msg + '\n'
+      msg = msg + ' '.repeat(2) + 'monster' + ' ' + monster.id + ' ' + '{' + '\n'
+      msg = msg + ' '.repeat(4) + 'position' + ' ' + '(' + (monster.pos.x).toString(10) + ' ' + (monster.pos.y).toString(10) + ')' + ';' + '\n'
+      msg = msg + ' '.repeat(4) + 'type' + ' ' + monster.type + ';' + '\n'
+      msg = msg + ' '.repeat(4) + 'direction' + ' ' + (monster.direction === '' ? 'DIR_LEFT' : monster.direction) + ';' + '\n'
+      msg = msg + ' '.repeat(2) + '}' + '\n'
+      body = body + msg
+    }
+    for (const area of this.areas) {
+      let msg = ''
+      msg = msg + '\n'
+      msg = msg + ' '.repeat(2) + 'area' + ' ' + area.id + ' ' + '{' + '\n'
+      msg = msg + ' '.repeat(4) + 'position' + ' ' + '(' + (area.pos.x).toString(10) + ' ' + (area.pos.y).toString(10) + ')' + ';' + '\n'
+      msg = msg + ' '.repeat(4) + 'type' + ' ' + area.type + ';' + '\n'
+      msg = msg + ' '.repeat(4) + 'direction' + ' ' + (area.direction === '' ? 'DIR_LEFT' : area.direction) + ';' + '\n'
+      msg = msg + ' '.repeat(2) + '}' + '\n'
+      body = body + msg
+    }
+    const end = '}'
+    return start + body + end
+  }
 }
 
 export { DFMap }
