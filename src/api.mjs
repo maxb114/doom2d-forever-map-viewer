c4385792131faf623da98e7ab214ad5a5bee842a
Max Brylov <max.brylov@protonmail.com>
2022-06-04 20:07:22 +0300

    introduce basic api module

--- src/api.mjs
+++ src/api.mjs
@@ -1,6 +1,9 @@
+import { getCameraWrapper } from './main.mjs'
 
 function moveCameraByDelta(/** @type {number} */ deltaX, /** @type {number} */ deltaY) {
-  
+  const cameraWrapper = getCameraWrapper()
+  if (cameraWrapper === null) return
+  cameraWrapper.setCameraCoords(deltaX, deltaY)
 }
 
-export {moveCameraByDelta}
+export { moveCameraByDelta }
