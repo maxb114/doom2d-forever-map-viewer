4d639bb89d2a09bc555e0ca26c36901847687f83
Max Brylov <max.brylov@protonmail.com>
2022-06-04 21:19:32 +0300

    add setMapFromJSON to api

--- src/api.mjs
+++ src/api.mjs
@@ -1,4 +1,4 @@
-import { getCameraWrapper, getCurrentMapAsJSON } from './main.mjs'
+import { getCameraWrapper, getCurrentMapAsJSON, setCurrentMapFromJSON } from './main.mjs'
 
 function moveCameraByDelta (/** @type {number} */ deltaX, /** @type {number} */ deltaY) {
   const cameraWrapper = getCameraWrapper()
@@ -12,9 +12,13 @@ function moveCamera (/** @type {number} */ x, /** @type {number} */ y) {
   cameraWrapper.moveCameraToCoords(x, y)
 }
 
+function setMapFromJSON (/** @type {any} */ mapObject) {
+  setCurrentMapFromJSON(mapObject)
+}
+
 function currentMapAsJSON () {
   const /** @type {any} */ mapObject = getCurrentMapAsJSON()
   return mapObject
 }
 
-export { moveCameraByDelta, moveCamera, currentMapAsJSON }
+export { moveCameraByDelta, moveCamera, currentMapAsJSON, setMapFromJSON }
