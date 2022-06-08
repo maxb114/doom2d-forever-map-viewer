4178aba2c6c7de64546952f8dbf1b92e922142b4
Max Brylov <max.brylov@protonmail.com>
2022-06-08 16:55:24 +0300

    initial commit

--- src/clone-map.mjs
+++
@@ -1,9 +0,0 @@
-import { mapFromJson } from './map-from-json-parse.mjs'
-
-function cloneMap (/** @type {DFMap} */ map) {
-  const json = JSON.stringify(map)
-  const clone = mapFromJson(json)
-  return clone
-}
-
-export { cloneMap }
--- src/maps-equal.mjs
+++
@@ -1,6 +0,0 @@
-function mapsContentEqual (/** @type {any} */ firstMapObject, /** @type {any} */ secondMapObject) {
-  const test = firstMapObject === secondMapObject
-  return test
-}
-
-export { mapsContentEqual }
